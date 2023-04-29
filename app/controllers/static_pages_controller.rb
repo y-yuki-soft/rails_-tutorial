88a2dbbcf48f9edb8470560e9bbe3e603ea13f41
y-yuki-soft <130302510+y-yuki-soft@users.noreply.github.com>
2023-04-29 15:51:05 +0000

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,11 @@
 class StaticPagesController < ApplicationController
+    # app/views/コントラーラ名/アクション名.html.rb
   def home
   end
 
-  # app/views/コントラーラ名/アクション名.html.rb
   def help
+  end
+
+  def about
   end
 end
